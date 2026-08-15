f21d7b6d576f78c32941ec2505dfbfedde25ed32
Pixel Gang Studios <makecentz@yahoo.com>
2026-08-15 10:56:32 -0400

    Add transparent dark theme logo

--- components/brand/Logo.tsx
+++ components/brand/Logo.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export function Logo({ inverse = false }: { inverse?: boolean }) {
   return (
     <span className={`brand-logo ${inverse ? "brand-logo--inverse" : ""}`}>
-      <img src="/perkjoy-logo-transparent.png" alt="PerkJoy" />
+      <Image className="brand-logo-light" src="/perkjoy-logo-transparent.png" alt="PerkJoy" width={1920} height={1080} />
+      <Image className="brand-logo-dark" src="/perkjoy-logo-dark.png" alt="PerkJoy" width={1920} height={1080} />
     </span>
   );
 }
